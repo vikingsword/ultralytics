0b49d8a15c67f5d2f4ed32484f12e41675a14d7e
vikingar <423141759@qq.com>
2023-11-08 22:03:08 +0800

    test

--- test.py
+++ test.py
@@ -4,3 +4,12 @@ if torch.cuda.is_available():
     print("GPU is available")
 else:
     print("GPU is not available")
+
+# 创建一个 tensor
+tensor = torch.ones(2, 2)
+
+# 将 tensor 放在 GPU 上
+tensor = tensor.to('cuda')
+
+# 打印 tensor 的设备  # 输出结果应该是 cuda:0，表示 tensor 在第一个 GPU 上。
+print(tensor.device)
